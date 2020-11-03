b66f9f433dbe9eea825824854b3d778e5cea1dcd
jtrancos <jesus.trancoso.jimenez@gmail.com>
2020-11-03 14:04:16 +0100

    go biden go

--- lodev.c
+++ lodev.c
@@ -1,4 +1,6 @@
 #include "mlx/mlx.h"
+#include <stdlib.h>
+#include <math.h>
 
 #define mapwidth 24
 #define mapheight 24
@@ -31,10 +33,26 @@ typedef struct	s_data
 	int 	blockx;
 	int		blocky;
 	int		blocklen;
+	int		width;
 	t_player	player;
 	
 }				t_data;
 
+int		ft_close(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	exit(0);
+	return(0);
+}
+
+int		ft_escape(int keycode, t_data *data)
+{
+	if (keycode == 53)
+	{
+		ft_close(data);
+	}
+	return(0);
+}
 
 int worldmap [mapwidth][mapheight] = {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -63,9 +81,17 @@ int worldmap [mapwidth][mapheight] = {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
+/*
+** hola
+** esto
+** es
+** estopa 
+*/
+
 int main (int argc, char **argv)
 {
 	t_data data;
+	data.width = 640;
 	data.player.x = 22;
 	data.player.y = 12;
 	data.player.dir_x = -1;
@@ -74,6 +100,68 @@ int main (int argc, char **argv)
 	data.player.plane_y= 0.66;
 	data.player.time = 0;
 	data.player.old_time = 0;
+	int x = 0;
 	
-	data.mlx = mlx_new_window(data.win, screenwidth, screenheight, "raycaster");
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, screenwidth, screenheight, "raycaster");
+	while (x < screenwidth)
+	{
+		float camera_x = 2 * x / (float)screenwidth - 1;
+		float ray_dir_x = data.player.dir_x + data.player.plane_x * camera_x;
+		float ray_dir_y = data.player.dir_y + data.player.plane_y * camera_x;
+		int map_x = (int)data.player.x;
+		int map_y = (int)data.player.y;
+		float side_dist_x;
+		float side_dist_y;
+		float delta_dist_x = ray_dir_y == 0 ? 1 : fabs(1 / ray_dir_x);
+		float delta_dist_y = ray_dir_x == 0 ? 1 : fabs(1 / ray_dir_y);
+		float perpwalldist;
+		int step_x;
+		int step_y;
+		int hit = 0;
+		int side;
+
+		if (ray_dir_x < 0)
+		{
+			step_x = -1;
+			side_dist_x = (data.player.x - map_x) * delta_dist_x;
+		}
+		else
+		{
+			step_x = 1;
+			side_dist_x = (map_x + 1 - data.player.x) * delta_dist_x;
+		}
+		if (ray_dir_y < 0)
+		{
+			step_y = -1;
+			side_dist_y = (data.player.y - map_y) * delta_dist_y;
+		}
+		else
+		{
+			step_y = 1;
+			side_dist_y = (map_y + 1 - data.player.y) * delta_dist_y;
+		}
+		while (hit == 0)
+		{
+			if (side_dist_x < side_dist_y)
+			{
+				side_dist_x += delta_dist_x;
+				map_x += step_x;
+				side = 0;
+			}
+			else
+			{
+				side_dist_y += delta_dist_y;
+				map_y += step_y;
+				side = 1;
+			}
+			if (worldmap[map_x][map_y] > 0)
+				hit = 1;
+		}
+		
+		
+	
+	}
+	mlx_key_hook(data.win, ft_escape, &data);
+	mlx_loop(data.mlx);
 }
